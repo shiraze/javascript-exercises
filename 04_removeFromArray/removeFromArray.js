154e8015917020175b18c90cf01c3e6d93bebecf
Shiraz Esat <shiraz.esat@madetech.com>
2022-08-31 12:57:31 +0100

    Provide function to remove array items
    Takes an array, and a list of items to remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...excludes) {
+  return array.filter((item) => !excludes.includes(item));
 };
 
 // Do not edit below this line
