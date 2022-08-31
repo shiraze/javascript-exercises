120537e799ddd88dacc10a0b861fe02253279ab1
Shiraz Esat <shiraz.esat@madetech.com>
2022-08-31 13:07:22 +0100

    Provide function to sum numbers between two numbers
    Simple function, with minimal error checking.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  if (
+    start < 0 ||
+    end < 0 ||
+    typeof start != "number" ||
+    typeof end != "number"
+  )
+    return "ERROR";
 
+  var sum = 0;
+  // Note that only one of these loops will be run!
+  for (let index = start; index <= end; index++) {
+    sum += index;
+  }
+  for (let index = start; index >= end; index--) {
+    sum += index;
+  }
+  return sum;
 };
 
 // Do not edit below this line
