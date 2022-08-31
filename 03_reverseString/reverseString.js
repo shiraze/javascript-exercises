4bf033ca344b1e739c3540ebacacdfd5e9e72c1a
Shiraz Esat <shiraz.esat@madetech.com>
2022-08-31 12:44:46 +0100

    Provide string reversal function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  var result = "";
+  for (let index = string.length; index > 0; index--) {
+    result += string[index - 1];
+  }
+  return result;
 };
 
 // Do not edit below this line
